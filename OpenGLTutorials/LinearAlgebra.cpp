79de6f30b330be2660d4a91e734afc7111302a79
John <gtom_1984@hotmail.ca>
2021-04-02 23:30:23 -0700

    LinearAlgebra. Fix Matrix Multiplication
    - Create 1st version of matrix multiplication

--- OpenGLTutorials/LinearAlgebra.cpp
+++ OpenGLTutorials/LinearAlgebra.cpp
@@ -5,51 +5,55 @@
 void matrixMultiplication(float matrixA[], float matrixB[], float resultingMatrix[], int size) {
 
 		int index = 0;
-		int currentRow = 0;
-		int currentCol = 0;
 		int length = (int)sqrt((double)size);
 
-		printf("Length = %d\n", length);
-		for (int row = 0; row < length; row++) {
-
-			for (int col = 0; col < length; col++) {
+		for (int rowA = 0; rowA < length; rowA++) {
+			for (int colB = 0; colB < length; colB++) {
 				float result = 0;
 				for (int i = 0; i < length; i++) {
-					int column = col + (i * length);
-					result += matrixA[row + i] * matrixB[column];
+					int columnResult = colB + (i * length);
+					int rowResult = (rowA * length) + i;
+					result += matrixA[rowResult] * matrixB[columnResult];
 				}
 				resultingMatrix[index] = result;
 				index++;
-			}			
+			}
 		}
-	
 
-	// matrices are stored in column order:
+	// matrices are stored in column
+	// order:
 	/*
 		a b c d		f g h q
 		e f g h		s t y o
 		i j k l		q w z x
 		m n o p		l c v d
 	*/
-
 }
 
 
 void test() {
 	float matrixA[] = {
-		1.0f, 1.0f, 1.0f,
-		2.0f, 2.0f, 2.0f,
-		3.0f, 3.0f, 3.0f,
+		2.0f, 2.0f, 2.0f, 2.0f,
+		3.0f, 3.0f, 3.0f, 3.0f,
+		4.0f, 4.0f, 4.0f, 4.0f,
+		5.0f, 5.0f, 5.0f, 5.0f
 	};
 
-	float resultingMatrix[9];
+	float matrixB[] = {
+		3.0f, 3.0f, 3.0f, 3.0f,
+		6.0f, 6.0f, 6.0f, 6.0f,
+		7.0f, 7.0f, 7.0f, 7.0f,
+		1.0f, 1.0f, 1.0f, 1.0f
+	};
 
+
+	float resultingMatrix[16];
 	
-	matrixMultiplication(matrixA, matrixA, resultingMatrix, (int)sizeof(matrixA)/sizeof(matrixA[0]));
+	matrixMultiplication(matrixA, matrixB, resultingMatrix, (int)sizeof(matrixA)/sizeof(matrixA[0]));
 	
 	int size = sizeof(resultingMatrix) / sizeof(resultingMatrix[0]);
 	for (int i = 0; i < size; i++) {
 		printf("%f, ", resultingMatrix[i]);
 	}
-	printf("Size = %d", sizeof(resultingMatrix));
+	printf("Size = %d", size);
 }
