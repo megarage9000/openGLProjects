1930222a009ae05f8f9176afc0ea5838da620561
John <gtom_1984@hotmail.ca>
2024-12-01 09:35:41 -0800

    fix model import

--- GarageEngineSolution/MeshDemo/Model.cpp
+++ GarageEngineSolution/MeshDemo/Model.cpp
@@ -16,6 +16,7 @@ void Model::LoadModel(string path) {
 	stbi_set_flip_vertically_on_load(true);
 	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
 		cout << "ERROR\n";
+		cout << importer.GetErrorString();
 		return;
 	}
 
--- GarageEngineSolution/MeshDemo/main.cpp
+++ GarageEngineSolution/MeshDemo/main.cpp
@@ -79,7 +79,7 @@ int main() {
 		"mesh.frag");
 
 	// Define Model here
-	Model model{ "..\\testMeshes\\backpack\\backpack.mtl" };
+	Model model{ "..\/testMeshes\/backpack\/backpack.mtl" };
 
 	// Define projection matrix
 	Mat4 projection = set_up_projection_matrix();
